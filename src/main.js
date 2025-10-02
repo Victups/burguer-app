e92c8736d68cbc0c716f58c4eb8f968e7061d9f0
Victor Pereira <victorgabrielcapereira@gmail.com>
2025-10-02 15:57:45 -0300

    removendo vitify

--- src/main.js
+++ src/main.js
@@ -2,14 +2,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
-import 'vuetify/styles'
-import { createVuetify } from 'vuetify'
 
-import * as components from 'vuetify/components'
-import * as directives from 'vuetify/directives'
 
 createApp(App).use(router).mount('#app')
-const vuetify = createVuetify({
-    components,
-    directives,
-})
